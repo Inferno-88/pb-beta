9e151074030b8278bd214e86926f37a9c9c249cd
Deadlyfangs <4674059+Deadlyfangs@users.noreply.github.com>
2018-02-21 14:40:55 +0300

    Removed datasSource from config

--- src/main/java/com/octopus/pb/Application.java
+++ src/main/java/com/octopus/pb/Application.java
@@ -3,27 +3,30 @@ package com.octopus.pb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    WebMvcConfigurer configurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers (ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/events/**").
-                        addResourceLocations("classpath:/static/events/");
-            }
-        };
-    }
+//    @Bean
+//    WebMvcConfigurer configurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+//                registry.addResourceHandler("/events/**").
+//                        addResourceLocations("classpath:/static/events/");
+//            }
+//        };
+//    }
 
 }
