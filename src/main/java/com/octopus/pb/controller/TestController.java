1efab15961cde08ad3896c68d0b43b9cdaa43203
Deadlyfangs <4674059+Deadlyfangs@users.noreply.github.com>
2018-02-21 12:03:47 +0300

    First run with frontend

--- src/main/java/com/octopus/pb/controller/TestController.java
+++ src/main/java/com/octopus/pb/controller/TestController.java
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
+//    @RequestMapping("/")
+//    public String getIndex() {
+//
+//        System.out.println("LOL INIT");
+//
+//        return "forward:index.html";
+//    }
+
     @RequestMapping("/page")
     public String getPage() {
 
