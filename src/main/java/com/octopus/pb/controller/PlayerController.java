c08584f924d4f0810d37fd2d979f9ffd87984d45
Deadlyfangs <4674059+Deadlyfangs@users.noreply.github.com>
2018-06-05 09:03:33 +0300

    Working build 0.5

---
+++ src/main/java/com/octopus/pb/controller/PlayerController.java
@@ -0,0 +1,13 @@
+package com.octopus.pb.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+
+
+}
